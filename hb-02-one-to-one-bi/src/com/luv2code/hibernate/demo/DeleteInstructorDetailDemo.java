ecab0643e37d3dc72a296888b612eafe91a82011
VesnaK193 <vesna.karaklic.97@gmail.com>
2021-09-24 17:27:14 +0200

    Hibernate-one to one, one to many

---
+++ hb-02-one-to-one-bi/src/com/luv2code/hibernate/demo/DeleteInstructorDetailDemo.java
@@ -0,0 +1,63 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+
+public class DeleteInstructorDetailDemo {
+
+	public static void main(String[] args) {
+		
+		//create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.buildSessionFactory();
+		
+		//create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			
+			//start a transaction
+			session.beginTransaction();
+			
+			//get the instructor detail object
+			int theId = 3;
+			InstructorDetail tempInstructorDetail = session.get(InstructorDetail.class, theId);
+			
+			//print the instructor detail
+			//System.out.println("tempInstructorDetail: " + tempInstructorDetail);
+			System.out.println("tempInstructorDetail: " + tempInstructorDetail.getId());
+			
+			//print the associated instructor
+			System.out.println("The associated instructor: " + tempInstructorDetail.getInstructor().getFirstName());
+			
+			//now let-s delete the instructor detail
+			System.out.println("Deleting...");
+			
+			//remove the associated object reference
+			//break bi-directional link
+			tempInstructorDetail.getInstructor().setInstructorDetailId(null);
+			
+			session.delete(tempInstructorDetail);
+			
+			//commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+			
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		
+		} finally {
+			factory.close();
+		}
+		
+	}
+
+}
